6a612a836f90e98067ace3e281efa7f5443ae477
Timo Warns <timow+github@DiningPhilosopher.DE>
2015-03-12 22:02:38 +0100

    Add dispatchers for more management and EAP frames
    Dispatch beacons, probe requests, probe responses, authentication frames,
    association requests, association responses, and EAP ID requests.

--- src/dot11er/infra.py
+++ src/dot11er/infra.py
@@ -37,13 +37,71 @@ def RX_BEACON_QUEUE(mon_if):
     interface 'mon_if'."""
     return "%s.rx_beacon" % mon_if
 
-# TODO add more reasonable sanity checks
-"""Filter for beacon frames and publish them on queue
-'RX_BEACON_QUEUE(mon_if)'."""
-rx_beacon = functools.partial(simple_filter, \
-        IN_QUEUE = RX_FRAME_QUEUE, OUT_QUEUE = RX_BEACON_QUEUE, \
-        filt = lambda f: f.type == Dot11.TYPE_MANAGEMENT and \
-            f.subtype == Dot11.SUBTYPE["Management"]["Beacon"])
+def RX_PROBE_QUEUE(mon_if):
+    """Return name of queue used for probe request frames received on monitoring
+    interface 'mon_if'."""
+    return "%s.rx_probe" % mon_if
+
+def RX_PROBE_RESP_QUEUE(mon_if):
+    """Return name of queue used for probe response frames received on
+    monitoring interface 'mon_if'."""
+    return "%s.rx_probe_resp" % mon_if
+
+def RX_AUTH_QUEUE(mon_if):
+    """Return name of queue used for authentication frames received on
+    monitoring interface 'mon_if'."""
+    return "%s.rx_auth" % mon_if
+
+def RX_ASSOC_QUEUE(mon_if):
+    """Return name of queue used for association request frames received on
+    monitoring interface 'mon_if'."""
+    return "%s.rx_assoc" % mon_if
+
+def RX_ASSOC_RESP_QUEUE(mon_if):
+    """Return name of queue used for association response frames received on
+    monitoring interface 'mon_if'."""
+    return "%s.rx_assoc_resp" % mon_if
+
+def rx_dispatcher(r, mon_if):
+    """Dispatch received frames to rx queues."""
+
+    mng_queue = {
+        Dot11.SUBTYPE["Management"]["Beacon"]               : RX_BEACON_QUEUE(mon_if),
+        Dot11.SUBTYPE["Management"]["Probe Request"]        : RX_PROBE_QUEUE(mon_if),
+        Dot11.SUBTYPE["Management"]["Probe Response"]       : RX_PROBE_RESP_QUEUE(mon_if),
+        Dot11.SUBTYPE["Management"]["Authentication"]       : RX_AUTH_QUEUE(mon_if),
+        Dot11.SUBTYPE["Management"]["Association Request"]  : RX_ASSOC_QUEUE(mon_if),
+        Dot11.SUBTYPE["Management"]["Association Response"] : RX_ASSOC_RESP_QUEUE(mon_if),
+        }
+
+    ps = r.pubsub()
+    ps.subscribe(RX_FRAME_QUEUE(mon_if))
+    for m in ps.listen():
+        f = frame(m)
+
+        # TODO add more reasonable sanity checks
+        if f.type == Dot11.TYPE_MANAGEMENT and mng_queue.has_key(f.subtype):
+            r.publish(mng_queue[f.subtype], f)
+
+def RX_EAP_ID_QUEUE(mon_if):
+    """Return name of queue used for EAP ID request frames received on
+    monitoring interface 'mon_if'."""
+    return "%s.rx_eap_id" % mon_if
+
+def rx_eap_dispatcher(r, mon_if):
+    """Dispatch received EAP frames to EAP queues."""
+    ps = r.pubsub()
+    ps.subscribe(RX_FRAME_QUEUE(mon_if))
+
+    for m in ps.listen():
+        f = frame(m)
+
+        if f.haslayer(Dot11) and \
+                f.type == Dot11.TYPE_DATA and \
+                f.haslayer(EAP):
+            eap = f[EAP]
+            if eap.code == EAP.REQUEST and eap.type == EAP.TYPE_ID:
+                r.publish(RX_EAP_ID_QUEUE(mon_if), f)
 
 def AP_QUEUE(mon_if):
     """Return name of queue used for APs detected on monitoring interface
