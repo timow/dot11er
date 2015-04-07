b1699cd552502efcd6e6cef419d74ae6d7f3f679
Timo Warns <timow+github@DiningPhilosopher.DE>
2015-04-07 20:47:24 +0200

    omit starting rx_beacon (replaced by rx_dispatcher)

--- bin/ap_dump.py
+++ bin/ap_dump.py
@@ -3,7 +3,7 @@
 import sys
 import redis
 
-from dot11er.infra import AP_QUEUE,ap_dump,rx_beacon
+from dot11er.infra import AP_QUEUE,ap_dump
 from dot11er.util import start_process
 
 if __name__ == '__main__':
@@ -18,9 +18,6 @@ if __name__ == '__main__':
     p_ap_dump = start_process(ap_dump, ( \
             redis.StrictRedis(redis_host, redis_port, redis_db), \
             mon_if))
-    p_rx_beacon = start_process(rx_beacon, ( \
-            redis.StrictRedis(redis_host, redis_port, redis_db), \
-            mon_if))
 
     r = redis.StrictRedis(redis_host, redis_port, redis_db)
     ps = r.pubsub()
@@ -28,5 +25,4 @@ if __name__ == '__main__':
     for m in ps.listen():
         print m['data']
 
-    p_rx_beacon.join()
     p_ap_dump.join()
