ca9a3cfe267c68b14ba54d5dd42e26af2db6d3b2
Timo Warns <timow+github@DiningPhilosopher.DE>
2015-05-23 17:22:07 +0200

    make use of redis_obj helper function

--- bin/basic_services.py
+++ bin/basic_services.py
@@ -4,22 +4,20 @@ import sys
 import redis
 
 from dot11er.infra import *
-from dot11er.util import start_process, default_arg_parser
+from dot11er.state_machine import probe_request
+from dot11er.util import start_process, default_arg_parser, redis_obj
 
 if __name__ == '__main__':
     parser = default_arg_parser()
     parser.description = 'Run basic services (frame-to-queue dispatch, probe request).'
     args = parser.parse_args()
 
-    p_rx_dispatcher = start_process(rx_dispatcher, ( \
-            redis.StrictRedis(args.redis_host, args.redis_port, args.redis_db), \
-            args.mon_if))
-    p_rx_eap_dispatcher = start_process(rx_eap_dispatcher, ( \
-            redis.StrictRedis(args.redis_host, args.redis_port, args.redis_db), \
-            args.mon_if))
-    p_probe_request = start_process(probe_request, ( \
-            redis.StrictRedis(args.redis_host, args.redis_port, args.redis_db), \
-            args.mon_if))
+    p_rx_dispatcher = start_process(rx_dispatcher, \
+            (redis_obj(args), args.mon_if))
+    p_rx_eap_dispatcher = start_process(rx_eap_dispatcher, \
+            (redis_obj(args), args.mon_if))
+    p_probe_request = start_process(probe_request, \
+            (redis_obj(args), args.mon_if))
 
     p_rx_dispatcher.join()
     p_rx_eap_dispatcher.join()
